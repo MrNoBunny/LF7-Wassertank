0379cb1dcbadce52fa98cfc72fec81b024cd8e62
MrNoBunny <145970434+MrNoBunny@users.noreply.github.com>
2023-09-25 11:41:57 +0200

    Update arduino-to-pi.py

--- arduino-to-pi.py
+++ arduino-to-pi.py
@@ -4,7 +4,7 @@ import time
 if __name__ == '__main__':
   ser = serial.Serial( '/dev/ttyACM0' , 9600, timeout=1)
   ser.flush()
-  
+
   while True:
     if ser.in_waiting > 0:
       line = ser.readline().decode('utf-8').rstrip()
@@ -12,3 +12,5 @@ if __name__ == '__main__':
       if(float(line)>30):
         print("Zu heiß!")
         ser.write(b"HOT\n")
+
+#testen ob es pushed
