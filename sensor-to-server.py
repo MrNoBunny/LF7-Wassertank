a266132aeb007e0c1f9a528649bd31fa42a10df5
Lennart A <lennij@posteo.de>
2023-09-26 12:55:59 +0200

    sensor to server angepasst

--- sensor-to-server.py
+++ sensor-to-server.py
@@ -1,36 +1,44 @@
 # Wichtige Bibliothek
 import socket
-
 import serial
-import time
+#import time
 
 if __name__ == '__main__':
   #Serielle Verbindung zum Arduino erstellen
   ser = serial.Serial( '/dev/ttyACM0' , 9600, timeout=1)
   ser.flush()
-
+  
   #Socket/UDP-Verbindung zum Server erstellen
   serverAddress = ('192.168.123.10' , 2222)
   bufferSize = 1024
   UDPClient = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
-
+  
   while True:
+    # Serial Connection auslesen
     if ser.in_waiting > 0:
       line = ser.readline().decode('utf-8').rstrip()
+
+      # Daten auswerten
       daten = line.split(", ")
-      print("Wasserdetektor:",daten[0])
-      print("Wasserstand:",daten[1])
-
-      # Anfrage vom Client starten
-      #msgFromClient ='Moin hier der Client'
-      #bytesToSend = msgFromClient.encode( 'utf-8' )
-      bytesToSend = line.encode( 'utf-8'  )
-      UDPClient.sendto(bytesToSend, serverAddress )
-
-      if(float(daten[0])>30):
-        print("Zu heiß!")
-        ser.write(b"HOT\n ")
-
+      if (float(daten[0]) > 100):
+        regen = True
+      else:
+        regen = False
+        
+      # Daten lokal anzeigen
+      print("Regnen:", regen)
+      print("Wasserstand:",daten[1]+"cm Luft im Tank")
+      
+      # Daten zum Server senden
+      bytesToSend = str(regen)+", "+daten[1]
+      bytesToSend = bytesToSend.encode('utf-8')
+      UDPClient.sendto(bytesToSend, serverAddress)
+      
+      # Daten an Arduino senden
+      if (float(daten[1])<5) or (float(daten[1])>100):
+        print("Tank voll")
+        ser.write(b"WARNING\n ")
+      
 # Warten auf Antwort
 #data,address =UDPClient.recvfrom(bufferSize)
 #Daten vom Server verarbeiten bzw. ausgeben..
