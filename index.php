58044e4efa3decb096e13572f330ec880d856303
Lennart A <lennij@posteo.de>
2023-09-29 12:24:19 +0200

    php Website hinzugefügt

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+// Database connection details
+$host = "localhost";  // Replace with your database host
+$username = "tankadmin"; // Replace with your database username
+$password = "habehunger"; // Replace with your database password
+$database = "tanks"; // Replace with your database name
+
+// Create a database connection
+$connection = new mysqli($host, $username, $password, $database);
+
+// Check if the connection was successful
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+// SQL query to retrieve data from the "tanks" table
+$sql = "SELECT tank1, Wassermenge1, tank2, Wassermenge2, timestamp FROM tanks";
+
+// Execute the query
+$result = $connection->query($sql);
+
+// Check if there are any rows in the result   <th></th>
+if ($result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr><th>Wasserstand 1 in cm</th><th>Wassermenge 1 in ml</th><th>Wasserstand 2 in cm</th><th>Wassermenge 2 in ml</th><th>Timestamp</th></tr>";
+
+    // Fetch and display data
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["tank1"] . "</td><td>" . $row["Wassermenge1"] . "</td>
+              <td>" . $row["tank2"] . "</td><td>" . $row["Wassermenge2"] . "</td>
+              <td>" . $row["timestamp"] . "</td></tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No data available.";
+}
+
+// Close the database connection
+$connection->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+ .header-image {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+        }
+    </style>
+</head>
+<body>
+    <img src="LF7_Logo_Ecke.png" alt="LF7" class="header-image">
+    
+    <?php
+    // Display the data initially
+    fetchData($connection);
+    ?>
+</body>
+</html>
