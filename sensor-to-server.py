7e1ac7694f6ab78bbe65e3a54d188457db69fae5
Lennart A <lennij@posteo.de>
2023-09-29 14:50:53 +0200

    pumpen angepasst

--- sensor-to-server.py
+++ sensor-to-server.py
@@ -4,8 +4,13 @@ import serial # für Serial-Verbindung zum Arduino
 import time # für sleep Funktion
 import threading # um zwei loops gleichzeitig laufen zu lassen
 
+pumpe1 = False
+pumpe2 = False
+
 # Funktion zum Empfangen der Daten vom Arduino und weiterleiten an den Server
 def arduino_pi_pi():
+  global pumpe1
+  global pumpe2
   #Socket/UDP-Verbindung zum Server erstellen
   serverAddress = ('192.168.123.10' , 2222)
   UDPClient = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
@@ -36,9 +41,14 @@ def arduino_pi_pi():
       UDPClient.sendto(bytesToSend, serverAddress)
 
       # Daten an Arduino senden
-      if not (int(daten[0])<4) and not ((int(daten[3])<4) or (int(daten[3])>20)):
+      if not (int(daten[0])<5) and not ((int(daten[3])<4) or (int(daten[3])>20)) and not pumpe2 and not pumpe1:
         print("Pumpen!")
-        ser.write(b"PUMPEN\n ")
+        ser.write(b"PUMPEN_1")
+        pumpe1 = True
+      if (int(daten[0])<4) and not pumpe2 and pumpe1:
+        print("Pumpe 1 stopp!")
+        ser.write(b"\nSTOPP\n ")
+        pumpe1 = False
       #if (float(daten[1])<5) or (float(daten[1])>40):
       #  print("Tank 2 voll")
       #  ser.write(b"WARNING\n ")
@@ -46,6 +56,7 @@ def arduino_pi_pi():
 
 # Funktion zum Empfangen von Befehlen vom Server und weiterleiten an den Arduino
 def pi_pi_arduino():
+  global pumpe2
   # Serveradreese definieren
   ServerPort = 2222
   ServerIP = '192.168.123.20'
@@ -61,10 +72,11 @@ def pi_pi_arduino():
     # Befehl zum Pumpen an Arduino weiterleiten
     if befehl == "PUMPEN":
       ser.write(b"PUMPEN_2")
+      pumpe2 = True
     if befehl == "STOPP":
       ser.write(b"\nSTOPP\n ")
+      pumpe2 = False
       
-
 if __name__ == '__main__':
   #Serielle Verbindung zum Arduino erstellen
   ser = serial.Serial( '/dev/ttyACM0' , 9600, timeout=1)
